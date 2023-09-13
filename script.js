7c1f0bdc96f2721f150880ad147944706e7bb57e
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-09-14 00:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+let result={};
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let articlelessarray=[];
+let articlelessitem=[];
+for(let i=0;i<touristSpots.length;i++){
+	articlelessitem= touristSpots[i].replace(/\bthe\b | \ban\b | \ba\b /gi," ").trim();
+	articlelessarray.push(articlelessitem);
+	result[articlelessitem]=touristSpots[i];
+}
+articlelessarray.sort();
+let ans=[];
+for(let i of articlelessarray){
+	ans.push(result[i]);
+}
+console.log(ans);
